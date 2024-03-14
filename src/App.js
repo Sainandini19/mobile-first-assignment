2722a96e1e501a55b1dee539dee6cdddbca81808
Sainandini Bandari <sainadhini19@gmail.com>
2024-03-14 11:54:56 +0530

    Mobile Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './Login';
+import HomePage from './Homepage';
 
-function App() {
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+ 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route  path='/login' exact Component={Login} />
+     
+      <Route path='/' exact Component={HomePage} />
+       
+       
+       
+    </Routes>
+    </BrowserRouter>
+        
+    </>
+  )
 }
 
-export default App;
+export default App
